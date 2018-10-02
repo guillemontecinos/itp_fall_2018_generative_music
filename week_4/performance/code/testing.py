21a4cb7dc4747a4bcc2e6d092a65ea16e99f5f32
gamontec <gamontec@uc.cl>
2018-10-02 19:29:25 -0400

    update note assign work

--- week_4/performance/code/testing.py
+++ week_4/performance/code/testing.py
@@ -9,29 +9,13 @@ from scipy import stats
 
 csv_input = '../data/embalse_bocamina.csv'
 
+c_maj = [60,62,64,65,67,69,71,72]
+
 with open(csv_input, 'rb') as csvfile:
     data = csv.reader(csvfile)
     generation = []
     for column in data:
         generation.append(column)
-
-    # for column in generation[0]:
-    # column = 0
-    # while column < len(generation[0]):
-        # print generation[0][column]
-        # if generation[0][column] == "Bocamina_II":
-        #     print "Bocamina!"
-        # else:
-        #     print "Not Bocamina :()"
-        # column = column + 1
-
-    # print generation[0]
-
-    # i = 1
-    # while i < len(generation):
-    #     time = int(generation[i][0])
-    #     i+=1
-    #     print time
 
 def get_stats(data, header):
     # based on ufo_by_date.py get_stats function
@@ -62,36 +46,19 @@ def get_stats(data, header):
         "stdev": stdev
     }
 
-def note_assign(plant, central_note, scale, number_of_notes, data_set):
+def note_assign(plant, octave, scale, number_of_notes, data_set):
+    # plant: string corresponding to the header of the data_set
+    # octave: distance in octaves from central C (Midi: 60)
+    # scale: a list with the used scale, centered in central C
+    # number_of_notes: range of noted assigned to the data_set
     stats = get_stats(data_set, plant)
     mean = stats['mean']
     stdev = stats['stdev']
     set = []
-    set_len = 6/number_of_notes
-    i = 0
-    while i < number_of_notes:
-        # set[i] = [- 3 * stats['stdev'] + i * set_len * stats['stdev'], - 3 * stats['stdev'] + (i + 1) * set_len * stats['stdev']]
-        set[i] = [stdev * (-3 + i * l), stdev * (-3 + (i + 1) * l)]
-        i = i + 1
-    print set
-    # each sets entry corresponds to one note, and each entry will have a list that will define each range
-    if scale == "major":
-        # for energy in data_set:
-        print "Scale is major"
+    set_len = 6/float(number_of_notes)
+    for i in range(number_of_notes):
+        set.append([stdev * (-3 + i * set_len), stdev * (-3 + (i + 1) * set_len)])
+    print len(set)
+    
 
-    else:
-        print "Scale is not major"
-        # break
-
-
-number_of_notes = 10
-stats = get_stats(generation, 'Bocamina_II')
-mean = stats['mean']
-stdev = stats['stdev']
-set = []
-set_len = 6/float(number_of_notes)
-# print set_len
-# print stdev
-for i in range(number_of_notes):
-    set.append([stdev * (-3 + i * set_len), stdev * (-3 + (i + 1) * set_len)])
-print set
+note_assign("Bocamina_II", 20, "major", 10, generation)
