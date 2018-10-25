e3c1f4a9dc790521151cdfd3886d739198cc7f8d
gamontec <gamontec@uc.cl>
2018-10-25 12:54:41 -0400

    create a file of midi with a time quotation

--- week_6/process_midi.py
+++ week_6/process_midi.py
@@ -8,30 +8,6 @@ for i, track in enumerate(mid.tracks):
 
 # create array of notes
 notes = []
-# for message in mid.tracks[4]:
-#     print message
-#     note = ""
-#     time = ""
-#     if message.type == 'note_on':
-#         message_components = str(message).split(' ')
-#         for item in message_components:
-#             if 'note=' in item:
-#                 # notes.append(item.split('note=')[1])
-#                 note = item.split('note=')[1]
-#     elif message.type == 'note_off':
-#         message_components = str(message).split(' ')
-#         for item in message_components:
-#             if 'time=' in item:
-#                 # notes.append(item.split('time=')[1])
-#                 time = item.split('time=')[1]
-#     notes.append(note + "_" + time)
-# notes = ' '.join(notes)
-#
-# # write notes to text file
-# note_file = open("./assets/notes.txt", "w")
-# note_file.write(notes)
-# note_file.close()
-
 messages = []
 for message in mid.tracks[4]:
     messages.append(message)
